5c471fdb5c6a2efd81ddce3215823dab98157cab
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-10-04 23:33:16 +0900

    fixed a little

--- client/src/components/searching.js
+++ client/src/components/searching.js
@@ -5,14 +5,24 @@ import { connect } from 'react-redux';
 
 class Searching extends Component {
 
+	renderAccount(){
+		switch(this.props.auth) {
+			case null:
+				return;
+			case false:
+				return(
+					<h3 className="spaceTop spaceBottom" id="header">Login with the top right toolbar to save searches</h3>
+				);
+			default:
+				return;
+		}
+	}
+
 	render(){
 		return(
 			<div>
 				<h2 className="spaceTop spaceBottom">Search Tokyo for Apartments</h2>
-				{this.props.auth == null 
-	            	? <h3 className="spaceTop spaceBottom" id="header">Login with the top right toolbar to save searches</h3>
-		            : null
-			    }
+				{this.renderAccount()}
 				<GoogleMap />
 				<SelectBoxes />
 			</div>
